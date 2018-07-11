1fdaac35c2c544d34626ff514ce4b5613a4243b4
antonyboivin <antonyboivin@outlook.fr>
2018-07-11 19:37:13 +0200

    Step 2 : Create a new Django project

---
+++ PurBeurre/substitution_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader
+
+def home_page(request):
+    template = loader.get_template('substitution_app/home_page.html')
+    return HttpResponse(template.render(request=request))
