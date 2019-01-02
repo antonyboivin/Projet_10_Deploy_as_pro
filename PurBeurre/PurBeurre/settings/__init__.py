8f1b428a518e523a0f85074b0453ba6726f6ee37
antonyboivin <antonyboivin@outlook.fr>
2019-01-02 22:31:03 +0100

    new settings configuration

--- PurBeurre/PurBeurre/settings/__init__.py
+++ PurBeurre/PurBeurre/settings/__init__.py
@@ -25,12 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')7pb-xz9&-^w$$mnmpvpg03*tp3l2@%c2_)zkipu)9l)$x!nn+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'purbeurreantonyboivin.herokuapp.com', '142.93.170.125']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -66,7 +63,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # Add templates/ folder to the root. It contains project-specific templates
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, '../templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -86,12 +83,24 @@ WSGI_APPLICATION = 'PurBeurre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Adaptateur postgresql
         'NAME': 'purbeurre',
         'USER': 'antonyb',
         'PASSWORD': '0+0=LaTeteàT0T0',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'purbeurre',
+        'USER': 'antony',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -157,11 +166,11 @@ LOGIN_URL = '/connection/'
     # Static files settings
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '../staticfiles')
 
     # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '../static'),
     )
 
     # Simplified static file serving.
