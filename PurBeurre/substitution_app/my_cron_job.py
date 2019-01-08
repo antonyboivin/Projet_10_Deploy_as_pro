658ed4f4fe6522d8a012252bac8ed8cb095f7fd2
antonyboivin <antonyboivin@outlook.fr>
2019-01-08 18:08:32 +0100

    Add Cron Task

--- PurBeurre/substitution_app/my_cron_job.py
+++ PurBeurre/substitution_app/my_cron_job.py
@@ -5,8 +5,10 @@ class MyCronJob(CronJobBase):
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'substitution_app.my_cron_job'
-
+"""
+    # Test code
     def do(self):
         f= open("testCron.txt","a+")
         f.write("Un truc")
         f.close()
+"""
